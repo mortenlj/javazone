297a890b05256c8f5938c3f4a213f5828e73889b
Morten Lied Johansen <mortenjo@ifi.uio.no>
2023-10-26 09:23:02 +0200

    Add API ping endpoint

--- javazone/api/__init__.py
+++ javazone/api/__init__.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from . import v1
 
 router = APIRouter()
 router.include_router(v1.router, prefix="/v1")
+
+
+@router.get("/ping", status_code=status.HTTP_200_OK)
+def readiness():
+    return "pong"
