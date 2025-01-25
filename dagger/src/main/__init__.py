ac77d41c47d2947ff32b9f3f6af59c04dc50e48b
Morten Lied Johansen <mortenjo@ifi.uio.no>
2025-01-25 21:06:30 +0100

    No longer need source parameter as it can be embedded in dagger script

--- dagger/src/main/__init__.py
+++ dagger/src/main/__init__.py
@@ -2,11 +2,9 @@ import asyncio
 import textwrap
 from typing import Annotated
 
-from jinja2 import Template
-
 import dagger
-from dagger import dag, function, object_type, Doc
-
+from dagger import dag, function, object_type, DefaultPath, Ignore, Doc
+from jinja2 import Template
 
 RESEAL_SCRIPT = textwrap.dedent(
     """\
@@ -24,8 +22,10 @@ RESEAL_SCRIPT = textwrap.dedent(
 
 @object_type
 class Javazone:
+    source: Annotated[dagger.Directory, DefaultPath("/"), Ignore(["target", ".github", "dagger", ".idea"])]
+
     @function
-    def deps(self, source: dagger.Directory, platform: dagger.Platform | None = None) -> dagger.Container:
+    def deps(self, platform: dagger.Platform | None = None) -> dagger.Container:
         """Install dependencies in a container"""
         return (
             dag.container(platform=platform)
@@ -33,19 +33,19 @@ class Javazone:
             .with_workdir("/app")
             .with_exec(["pip", "install", "poetry"])
             .with_env_variable("POETRY_VIRTUALENVS_IN_PROJECT", "true")
-            .with_file("/app/pyproject.toml", source.file("pyproject.toml"))
-            .with_file("/app/poetry.lock", source.file("poetry.lock"))
+            .with_file("/app/pyproject.toml", self.source.file("pyproject.toml"))
+            .with_file("/app/poetry.lock", self.source.file("poetry.lock"))
             .with_exec(["poetry", "install", "--only", "main", "--no-root", "--no-interaction"])
         )
 
     @function
-    def build(self, source: dagger.Directory, platform: dagger.Platform | None = None) -> dagger.Container:
+    def build(self, platform: dagger.Platform | None = None) -> dagger.Container:
         """Build the application"""
         return (
-            self.deps(source, platform)
-            .with_file("/app/.prospector.yaml", source.file(".prospector.yaml"))
-            .with_directory("/app/javazone", source.directory("javazone"))
-            .with_directory("/app/tests", source.directory("tests"))
+            self.deps(platform)
+            .with_file("/app/.prospector.yaml", self.source.file(".prospector.yaml"))
+            .with_directory("/app/javazone", self.source.directory("javazone"))
+            .with_directory("/app/tests", self.source.directory("tests"))
             .with_exec(["poetry", "install", "--no-interaction"])
             .with_exec(["poetry", "run", "black", "--check", "."])
             .with_exec(["poetry", "run", "prospector"])
@@ -53,10 +53,10 @@ class Javazone:
         )
 
     @function
-    def docker(self, source: dagger.Directory, platform: dagger.Platform | None = None) -> dagger.Container:
+    def docker(self, platform: dagger.Platform | None = None) -> dagger.Container:
         """Build the Docker container"""
-        deps = self.deps(source, platform)
-        src = self.build(source, platform)
+        deps = self.deps(platform)
+        src = self.build(platform)
         return (
             dag.container(platform=platform)
             .from_("python:3.12-slim")
@@ -68,9 +68,7 @@ class Javazone:
         )
 
     @function
-    async def publish(
-        self, source: dagger.Directory, image: str = "ttl.sh/mortenlj-javazone", version: str = "develop"
-    ) -> list[str]:
+    async def publish(self, image: str = "ttl.sh/mortenlj-javazone", version: str = "develop") -> list[str]:
         """Publish the application container after building and testing it on-the-fly"""
         platforms = [
             dagger.Platform("linux/amd64"),  # a.k.a. x86_64
@@ -81,17 +79,17 @@ class Javazone:
         for v in ["latest", version]:
             variants = []
             for platform in platforms:
-                variants.append(self.docker(source, platform))
+                variants.append(self.docker(platform))
             cos.append(manifest.publish(f"{image}:{v}", platform_variants=variants))
 
         return await asyncio.gather(*cos)
 
     @function
     async def assemble_manifests(
-        self, source: dagger.Directory, image: str = "ttl.sh/mortenlj-javazone", version: str = "develop"
+        self, image: str = "ttl.sh/mortenlj-javazone", version: str = "develop"
     ) -> dagger.File:
         """Assemble manifests"""
-        template_dir = source.directory("deploy")
+        template_dir = self.source.directory("deploy")
         documents = []
         for filepath in await template_dir.entries():
             src = await template_dir.file(filepath).contents()
@@ -108,7 +106,7 @@ class Javazone:
 
     @function
     async def reseal_secret(
-        self, source: dagger.Directory, kubeconfig: Annotated[dagger.Secret, Doc("kubeconfig file for the cluster")]
+        self, kubeconfig: Annotated[dagger.Secret, Doc("kubeconfig file for the cluster")]
     ) -> dagger.File:
         """Reseal the secret, using the kubeconfig file for the cluster"""
         return await (
@@ -117,7 +115,7 @@ class Javazone:
             .with_workdir("/reseal")
             .with_new_file("/root/.kube/config", await kubeconfig.plaintext())
             .with_new_file("reseal.sh", RESEAL_SCRIPT)
-            .with_file(".env", source.file(".env"))
+            .with_file(".env", self.source.file(".env"))
             .with_exec(["bash", "reseal.sh"])
             .file("sealed-secret.yaml")
         )
