900446b82b6ea0702fb6cfb7708ad0f346656485
Morten Lied Johansen <mortenjo@ifi.uio.no>
2025-08-07 14:46:44 +0200

    Remove unused ping endpoint

--- javazone/http/api/__init__.py
+++ javazone/http/api/__init__.py
@@ -4,8 +4,3 @@ from . import v1
 
 router = APIRouter()
 router.include_router(v1.router, prefix="/v1")
-
-
-@router.get("/ping", status_code=status.HTTP_200_OK)
-def readiness():
-    return "pong"
