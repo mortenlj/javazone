f235f468bae8dfcce89865f76148ce7f22c1e86e
Morten Lied Johansen <mortenjo@ifi.uio.no>
2025-08-18 15:57:53 +0200

    Add user sessions page

--- javazone/http/www/__init__.py
+++ javazone/http/www/__init__.py
@@ -5,7 +5,7 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
-from javazone.http.deps import get_db
+from javazone.http.deps import get_db, get_current_user
 from .widgets import router as widgets_router
 from .. import schemas
 from ...database import models
@@ -27,6 +27,31 @@ def index(request: Request):
 @router.get("/sessions", status_code=status.HTTP_200_OK, response_class=HTMLResponse)
 def sessions(request: Request, db: Session = Depends(get_db)):
     all_sessions = [schemas.Session.model_validate_json(s.data) for s in db.query(models.Session).all()]
+    return templates.TemplateResponse(
+        request=request,
+        name="sessions.html.j2",
+        context={
+            "sessions": _sort_sessions_by_day(all_sessions),
+        },
+    )
+
+
+@router.get("/user/sessions", status_code=status.HTTP_200_OK, response_class=HTMLResponse)
+def user_sessions(request: Request, user: schemas.User = Depends(get_current_user), db: Session = Depends(get_db)):
+    sess = [
+        schemas.Session.model_validate_json(s.data)
+        for s in db.query(models.Session).filter(models.Session.users.any(email=user.email)).all()
+    ]
+    return templates.TemplateResponse(
+        request=request,
+        name="sessions.html.j2",
+        context={
+            "sessions": _sort_sessions_by_day(sess),
+        },
+    )
+
+
+def _sort_sessions_by_day(all_sessions):
     sessions = {
         "tuesday": [],
         "wednesday": [],
@@ -42,23 +67,4 @@ def sessions(request: Request, db: Session = Depends(get_db)):
             sessions["wednesday"].append(session)
         elif session.start_time.weekday() == 3:
             sessions["thursday"].append(session)
-    return templates.TemplateResponse(
-        request=request,
-        name="sessions.html.j2",
-        context={
-            "sessions": sessions,
-        },
-    )
-
-
-@router.get("/user/sessions", status_code=status.HTTP_200_OK, response_class=HTMLResponse)
-def user_sessions(request: Request, db: Session = Depends(get_db)):
-    return templates.TemplateResponse(
-        request=request,
-        name="todo.html.j2",
-    )
-
-
-@router.get("/login", status_code=status.HTTP_200_OK, response_class=RedirectResponse)
-def login(request: Request):
-    return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
+    return sessions
