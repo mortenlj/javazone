9afe65a7e2be3cb244251f7182027bd04f36b7fe
Morten Lied Johansen <mortenjo@ifi.uio.no>
2025-08-19 12:00:43 +0200

    Revert "Revert choice to create "services""
    This reverts commit d0dd05eae6e06545f90418b8ec02b05a4e59bbf4.

--- javazone/http/api/v1/endpoints/users.py
+++ javazone/http/api/v1/endpoints/users.py
@@ -1,13 +1,9 @@
-import logging
-
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from javazone.database import models
 from javazone.http import schemas
 from javazone.http.deps import get_db, get_current_user, get_authenticated_user
-
-LOG = logging.getLogger(__name__)
+from javazone.services import users
 
 router = APIRouter(
     responses={status.HTTP_404_NOT_FOUND: {"detail": "Not found"}},
@@ -16,14 +12,7 @@ router = APIRouter(
 
 @router.post("/", response_model=schemas.User, name="Create user", status_code=status.HTTP_201_CREATED)
 def post_user(user: schemas.AuthenticatedUser = Depends(get_authenticated_user), db: Session = Depends(get_db)):
-    db_user = models.User(
-        email=user.email, name=user.name, picture_url=str(user.picture_url) if user.picture_url else None
-    )
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    LOG.info("Created user %r", user)
-    return db_user
+    return users.create_user(user, db)
 
 
 @router.get("/me", response_model=schemas.User)
@@ -33,5 +22,4 @@ def get_me(user: schemas.User = Depends(get_current_user)):
 
 @router.delete("/me", name="Delete user", status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(user: schemas.User = Depends(get_current_user), db: Session = Depends(get_db)):
-    db.delete(user)
-    db.commit()
+    users.delete_user(user, db)
---
+++ javazone/services/users.py
@@ -0,0 +1,29 @@
+import logging
+from typing import Optional
+
+from sqlalchemy.orm import Session
+
+from javazone.database import models
+from javazone.http import schemas
+
+LOG = logging.getLogger(__name__)
+
+
+def create_user(user: schemas.AuthenticatedUser, db: Session) -> models.User:
+    db_user = models.User(
+        email=user.email, name=user.name, picture_url=str(user.picture_url) if user.picture_url else None
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    LOG.info("Created user %r", user)
+    return db_user
+
+
+def get_user(user: schemas.AuthenticatedUser, db: Session) -> Optional[models.User]:
+    return db.query(models.User).filter(models.User.email == user.email).first()
+
+
+def delete_user(user: schemas.User, db: Session):
+    db.delete(user)
+    db.commit()
